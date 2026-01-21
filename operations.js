a1ce59be2237bc442eed84bd16e7433d6db76d77
Mariam <codegirlbxl@gmail.com>
2026-01-21 22:33:04 +0100

    Ajout de la function power

--- operations.js
+++ operations.js
@@ -1,18 +1,21 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        return "Erreur: division par zéro";
-    }
-    return a / b;
+  if (b === 0) {
+    return "Erreur: division par zéro";
+  }
+  return a / b;
 }
 
+function power(a, b) {
+  return Math.pow(a, b);
+}
